2151cc3faac779908af7f1d71ce5455b2bed3e7d
Henrik Nordquist <hno@casalogic.dk>
2016-12-13 10:51:43 +0100

    Add trait, add structure to various files

--- src/Facades/HeyLoyalty.php
+++ src/Facades/HeyLoyalty.php
@@ -1,12 +1,10 @@
- <?php
-
+<?php
 namespace Sydgren\HeyLoyalty\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 class Dog extends Facade
 {
-
     protected static function getFacadeAccessor()
     {
         return 'HeyLoyalty';
